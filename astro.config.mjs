a1fc04ef087add363f4a9c1f16dd436d71df2f43
Alex <alex.devenrgy@gmail.com>
2024-09-07 22:51:54 +0300

    feat: add header and hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from '@astrojs/tailwind'
 
 import solidJs from '@astrojs/solid-js'
 
+import icon from 'astro-icon'
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind({
 		applyBaseStyles: false,
-	}), solidJs()],
+	}), solidJs(), icon()],
 })
-
