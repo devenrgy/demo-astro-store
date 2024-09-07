1c439b147e6755e680d19e1c3fe3dc7c3e6db1bd
Alex <alex.devenrgy@gmail.com>
2024-09-08 00:58:17 +0300

    feat: content update for intro block

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
 	astro: true,
 	typescript: true,
+	formatters: {
+		css: true,
+	},
 	stylistic: {
 		indent: 'tab',
 		quotes: 'single',
